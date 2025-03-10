37ccb5bfe8f37ae0ba7f518299525dffbda9c662
Francisco Alvarez <francisco.alvarez.rabanal@gmail.com>
2025-03-10 19:59:28 +0100

    Token refresh improvement

--- getPGXTokenService.js
+++ getPGXTokenService.js
@@ -4,30 +4,83 @@
 const pgxConfig = require('./pgxconfig.js');
 const axios = require('axios');
 
+let token = null;
 
 
-    function run(key) {
-      return new Promise(async function(resolve, reject) {
-        let pgxServerURL = pgxConfig.pgxUrl + '/auth/token';
+function run(key) {
+  return new Promise(async function (resolve, reject) {
 
-        try {
-            let postData, req;
 
-            postData = { "username": pgxConfig.user, "password": pgxConfig.password, "createSession": true, "source": "nodejs","sessionId": ""};
 
-            const res = await axios.post(pgxServerURL, postData);
+    let pgxServerURL = pgxConfig.pgxUrl + '/auth/token';
 
-            resolve(JSON.stringify(res.data));
-        }
-        catch (err) {
-          console.error(err);
-          reject("error");
-        }
-        finally {
+    try {
 
-        }
-});
+      if (token) {
+        // existing token found
+        token = await refreshToken();
+
+      } else {
+        // request new token
+        token = await getToken()
+       
+      }
+
+      resolve(JSON.stringify(token.data));
+    }
+    catch (err) {
+      console.error(err);
+      reject("error");
+    }
+    finally {
+
+    }
+
+  });
 }
+
+
+async function getToken() {
+
+  let pgxServerURL = pgxConfig.pgxUrl + '/auth/token';
+  let postData, req;
+  let token = null;
+
+  try {
+    postData = { "username": pgxConfig.user, "password": pgxConfig.password, "createSession": true, "source": "nodejs", "sessionId": "" };
+
+    token = await axios.post(pgxServerURL, postData);
+  } catch (err) {
+    console.error(err);
+  }
+  finally {
+    return token;
+  }
+
+}
+
+
+async function refreshToken() {
+  let postData;
+  let pgxServerURL = pgxConfig.pgxUrl + '/auth/token';
+
+  try {
+    postData = { "token": token.data.access_token };
+    const res = await axios.put(pgxServerURL, postData);
+  }
+  catch (err) {
+    // some error happened
+    console.error(err);
+    // try to get a fresh token
+    token = await getToken();
+  }
+  finally {
+    return token;
+  }
+
+}
+
+
 
 module.exports = {
   run: run
